51d55e17a5092de1ef9156b664352dc981be7c1e
Erin B <34677254+ErinB97@users.noreply.github.com>
2020-04-19 12:09:47 +0100

    Added: Runge-Kutta and Error checks
    Runge-Kutta method added as per reference material- I used the Runge-Kutta videos by LearnChemE on Youtube to learn about RK method
    
    Error checks for dynamic model convergence and tolerance were added. In addition to the reference model I added an error check for temperatures not-converging through miscalculation by stopping the model when the values are calculated outside of the max/min initial temperatures.
    
    I next plan to work on creating a UI to allow user to more easily set-up and control the model.

--- ThermalConduction2D.py
+++ ThermalConduction2D.py
@@ -11,6 +11,7 @@ from matplotlib import pyplot as plt
 
 global Nx, Ny, Nt
 
+
 #---------------------------------Functions-----------------------------------
 def populateTopology(topology,material,T):
     '''
@@ -49,7 +50,8 @@ def showTop(topology,prop,t):
             visTop[itY,itX] = topology[t,itY,itX][prop]
             
     return visTop
-    
+
+
 
 #------------------------Material Physical Properties-------------------------
 
@@ -75,6 +77,9 @@ silver = {'T':0,'kT':629,'cp':233,'rho':10490,'BC':False}
 
 #-----------------------------Model Properties--------------------------------
 
+#method = 'Euler'
+method = 'RK'
+
 total_length = 1            #length in [m]
 total_width = 1           #width in [m]
 dx = 0.025                   #increment size in X and Y directions [m]
@@ -84,7 +89,7 @@ Nx = int(total_length / dx)      #number of increments in X & Y directiosn [-]
 Ny = int(total_width / dy)
 
 
-total_time = 250        #final time condition [s]
+total_time = 10        #final time condition [s]
 dt = 0.6                  #time interval [s]
 Nt = int(total_time / dt)    #number of increments in time
 
@@ -96,7 +101,9 @@ properties = {'T':0,'kT': 0,'cp':0,'rho':0,'BC':False} #dict of properties each 
 
                               #B.C: Boolean term to identify cells containing boundary condition
 
-ss_tol = 0.01 #tolerance for steady state calculation to solve to
+ss_tol = 0.01 #tolerance for steady state calculation (difference between max/min values between iterations)
+dyn_tol = 0.01 #tolerance for dynamic calculations (difference between max/min and s.s value)
+
 
 #------------------------------Creating Model--------------------------------
 #creating array to store results                        
@@ -125,7 +132,11 @@ topology = np.array([copy.deepcopy(topology) for itt in range(Nt)]) #duplicate f
 alphaF = lambda top: top[0,0,0]['kT']/(top[0,0,0]['cp']*top[0,0,0]['rho'])
 
 #     Function for FDM Euler approximation of the 2-D heat equation
-heat_eqn = lambda top, alpha, dt, dx, dy, itt, itx, ity: top[itt,ity,itx]['T'] + dt*alpha*( ((top[itt,ity-1,itx]['T']-2*top[itt,ity,itx]['T']+top[itt,ity+1,itx]['T'])/dx**2) + ((top[itt,ity,itx-1]['T']-2*top[itt,ity,itx]['T']+top[itt,ity,itx+1]['T'])/dy**2) )
+heat_eqn = lambda top, alpha, dt, dx, dy, itt, itx, ity: top[itt,ity,itx]['T'] + dt*alpha*( ((top[itt,ity-1,itx]['T']-2*top[itt,ity,itx]['T']+top[itt,ity+1,itx]['T'])/dy**2) + ((top[itt,ity,itx-1]['T']-2*top[itt,ity,itx]['T']+top[itt,ity,itx+1]['T'])/dx**2) )
+
+
+#     T''(x,y) for Runge-Kutta method
+RK_eqn = lambda T, alpha, dx, dy, itx, ity: alpha*( ((T[itx-1,ity]-2*T[itx,ity]+T[itx+1,ity])/dy**2) + ((T[itx,ity-1]-2*T[itx,ity]+T[itx,ity+1])/dx**2))
 
 #     FDM Heat equation re-arranged for steady state conditions where dT/dt = 0 
 ss_heat_eqn = lambda top, dx, dy, itx, ity: (dx**2*(top[ity-1,itx]+top[ity+1,itx])+dy**2*(top[ity,itx-1]+top[ity,itx+1]))/(2*(dx**2 + dy**2))
@@ -141,10 +152,15 @@ stabV = stab(dt,dx,dy,alphaV)  # stability constant of system
 
 stab_pass = False #Gatekeeper for stability check
 
-
+#Snapshot of initial temperatures for error checking later
+IC_frame = showTop(topology,'T',0)
+T_IC_max = np.max(np.max(IC_frame)) #greatest initial temperature
+T_IC_min = np.min(np.min(IC_frame)) #smallest initial temperature
 
 #-------------------------------~~Steady State~~------------------------------
 topo_ss = showTop(topology,'T',0) # assign initial conditions from time = 0
+# (steady-state values are independant of physical properties hence an array of temperatures is OK)
+
 topo_ss_next = topo_ss.copy() # stores the next iteration of the calculation
 
 max_ss_err = 1; min_ss_err = 1; # allocating error values
@@ -168,26 +184,105 @@ while (max_ss_err >= ss_tol) or (min_ss_err >= ss_tol):
 
 #-------------------------------~~Dynamic Section~~------------------------------
 # Checking model stability for solving
+    
 if stabV > 1:
     print('Stability condition is not met')
 else:
     print('Stability condition is met, model will be solved')
     stab_pass = True
-# FDM & Euler's Solution:
-    for itt in range(Nt-1):
-        
-        for itY in range(1,Ny-1):
-            
-            for itX in range(1,Nx-1):
-                
-                if topology[itt,itX,itY]['BC'] == False:
-                    topology[itt+1,itY,itX]['T'] = heat_eqn(topology,alphaV,dt,dx,dy,itt,itX,itY)
-                    
 
+# FDM & Euler's Solution:
+    
+    if method == 'Euler':
+        
+        euler_err_max = [] #creating error arrays
+        euler_err_min = []
+        
+        for itt in range(Nt-1):
+            
+            if itt > 0:
+                if (( 0 < euler_err_max[itt-1] <= dyn_tol) or (0 < euler_err_min[itt-1] <= dyn_tol)) :
+                    print('Euler converged to tolerance within %d iterations' %(itt-1))
+                    break     
+                  
+            for itY in range(1,Ny-1):
+                
+                for itX in range(1,Nx-1):
+                    
+                    if topology[itt,itX,itY]['BC'] == False:
+                        
+                        topology[itt+1,itY,itX]['T'] = heat_eqn(topology,alphaV,dt,dx,dy,itt,itX,itY)
+            
+            #Calculating and checking error
+                        
+            euler_frame = showTop(topology,'T',itt+1)
+            euler_diff =  euler_frame - topo_ss
+            
+            euler_err_max.append(np.round(np.abs(np.max(np.max(euler_diff))),5))
+            euler_err_min.append(np.round(np.abs(np.min(np.min(euler_diff))),5))
+            
+            if itt > 0:
+                # Checking for incorrect convergance
+                if ((euler_err_max[itt] == euler_err_max[itt-1] ) or (euler_err_min[itt] == euler_err_min[itt-1])) and (euler_err_max[itt] != 0):
+                    print('Euler Error: Solution converging outside steady state')
+                    stab_pass = False
+                    break
+            # Checking for an incorrect calculation if a temperautre is calculated outside the range of initial conditions    
+            if (np.max(np.max(euler_frame)) > T_IC_max) or (np.min(np.min(euler_frame)) < T_IC_min):
+                print('Euler Error: Solution not convering, temperature(s) outside initial conditions')
+                stab_pass = False
+                break
             
             
             
-#---------------------------------Results-------------------------------------
+# 2nd Order Runge-Kutta                      
+    elif method == 'RK':
+        
+        RK_err_max = [] # creating error arrays
+        RK_err_min = []
+        
+        for itt in range(Nt-1):
+            
+            if itt > 0:
+                if (( 0 < RK_err_max[itt-1] <= dyn_tol) or (0 < RK_err_min[itt-1] <= dyn_tol)) :
+                    print('RK converged to tolerance within %d iterations' %(itt-1))
+                    break     
+            
+            for itY in range(1,Ny-1):
+                
+                for itX in range(1,Nx-1):
+                   
+                    if topology[itt,itX,itY]['BC'] == False:                                         
+                        
+                        k1_T_vals = showTop(topology,'T',itt)
+                     
+                        k1 = RK_eqn(k1_T_vals, alphaV, dx, dy, itX, itY)
+                                
+                        k2_T_vals =  k1_T_vals + k1*dt
+                                    
+                        k2 = RK_eqn(k2_T_vals, alphaV, dx,dy ,itX ,itY) 
+                        
+                        topology[itt+1,itX,itY]['T'] = topology[itt,itX,itY]['T'] + (dt/2)*(k1 + k2)
+
+            #Calculating and checking error
+            
+            RK_frame = showTop(topology,'T',itt+1)
+            RK_diff =  RK_frame - topo_ss
+            
+            RK_err_max.append(np.round(np.abs(np.max(np.max(RK_diff))),5))
+            RK_err_min.append(np.round(np.abs(np.min(np.min(RK_diff))),5))
+            
+            if itt > 0:
+                # Checking for incorrect convergance
+                if ((RK_err_max[itt] == RK_err_max[itt-1] ) or (RK_err_min[itt] == RK_err_min[itt-1])) and (RK_err_max[itt] != 0):
+                    print('RK Error: Solution converging outside steady state')
+                    stab_pass = False
+                    break
+            # Checking for an incorrect calculation if a temperautre is calculated outside the range of initial conditions
+            if (np.max(np.max(RK_frame)) > T_IC_max) or (np.min(np.min(RK_frame)) < T_IC_min):
+                print('RK Error: Solution not convering, temperature(s) outside initial conditions')
+                stab_pass = False
+                break
 
 # Steady State Plot
                     
